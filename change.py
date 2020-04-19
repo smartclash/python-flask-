16bcb2d743cc78fcc53d6d1d0c72dc5728417bba
kaarthik <kaarthik@kaarthiks-MBP.Dlink>
2020-04-19 11:23:28 +0530

    All python stuffs and HTML (flask)

---
+++ change.py
@@ -0,0 +1,19 @@
+
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    headline = "Hello world"
+    return render_template("change.html" , headline = headline)
+
+@app.route("/")
+def bye():
+    headline = "Goodbye" 
+    return render_template("change.html" , headline = headline)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+
+
+
+
